462e06c0033a11437d9060910f0bd5e5b1380f07
Mari <masha.petrova.27012004@gmail.com>
2020-12-16 11:32:36 +0300

    Правка от 16.12.2020

--- lesson25.js
+++ lesson25.js
@@ -1,14 +1,13 @@
 let abc = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя";
-const changeLetter = (char, key) => {
-    let index = abc.indexOf(char);
+const changeLetter = (char, key = 3, index = abs.indexOf(char)) => {
     if (index !== -1) {
         let deff = index + key;
         if (deff >= 0 && deff < abc.length) {
             return abc[deff];
         } else if (deff < 0) {
-            return abc[abc.length + deff];
+            return changeLetter(char, 0, abc.length + deff);
         } else {
-            return abc[deff - abc.length];
+            return changeLetter(char, 0, abc.length - deff);
         }
     } else {
         return char;
@@ -17,12 +16,11 @@ const changeLetter = (char, key) => {
 
 const getN = (ma = 11, mi = 0) => Math.floor(Math.random() * (ma - mi) + mi);
 
-let newWord = "", key = getN(-50, 50);
-console.log(newWord);
 
-
-const cipher = (phrase, key = getN()) => {
 let newWord = "Привет, Николя!";
+
+let newWord = "", key = getN(-50, 50);
+const cipher = (phrase, key = getN()) => {
 for (let i = 0; i < phrase.length; i++) {
     newWord += changeLetter(phrase[i], key);
     }
